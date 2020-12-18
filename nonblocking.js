a23b45fa51c768d24c3f65fb172a846a2a666c0b
이다한 <da224@team-everywhere.com>
2020-12-18 12:44:47 +0900

    code upload

---
+++ nonblocking.js
@@ -0,0 +1,9 @@
+
+// console.log('call 1')
+
+setTimeout(() => {
+    console.log('call 1')
+}, 3000);
+
+
+console.log('call 2')
---
+++ system.js
@@ -0,0 +1,6 @@
+console.log(process)
+console.log(os)
+// arch: : 아키텍쳐
+// platform : darwin
+// env : 여러 환경설정
+// pid : 프로세스 아이디
